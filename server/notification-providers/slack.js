d5f56ebaf1d8ff0463b7e294f55ea837b6dfcee1
GJS <homelab.api@gmail.com>
2025-01-26 13:29:39 +0100

    Enhanced: Added UTC and timezone support to dayjs - Extended dayjs with `utc` and `timezone` plugins for accurate time formatting. - Ensured proper handling of time zone conversions and UTC time.
    modified: server/notification-providers/slack.js

--- server/notification-providers/slack.js
+++ server/notification-providers/slack.js
@@ -6,3 +6,8 @@ const { getMonitorRelativeURL } = require("../../src/util");
 const dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc");
 const timezone = require("dayjs/plugin/timezone");
+
+// Extend dayjs with UTC and timezone plugins for proper time formatting
+// This ensures that dayjs can handle time zone conversions and UTC time correctly.
+dayjs.extend(utc);
+dayjs.extend(timezone);
